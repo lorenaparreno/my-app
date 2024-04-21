07a319d54237e29964d83688eca4384cac34bce0
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-04-21 17:39:49 -0400

    initial changes

---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    let temperature = Math.round(response.data.main.temp);
+    alert(`The weather in ${response.data.name} is ${temperature}℃`);
+  }
+
+  let apiKey = "3a94f3778290bfeee61278505dbbe51d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
+}
